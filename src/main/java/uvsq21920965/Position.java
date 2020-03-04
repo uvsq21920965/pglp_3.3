1edf525c86f1899e2e27e55feda94dd9498abe2f
uvsq21920965 <sarra.belmahdi@gmail.com>
2020-03-04 17:04:11 +0100

    javadoc

--- src/main/java/uvsq21920965/Position.java
+++ src/main/java/uvsq21920965/Position.java
@@ -1,25 +1,53 @@
 package uvsq21920965;
-
+/**
+ * 
+ * @author sarra Belmahdi.
+ *
+ */
 public class Position {
+	/**
+	 * coordonnee x du position.
+	 */
 	private int x;
+	/**
+	 * coordonnee y du position.
+	 */
 	private int y;
-	
+	/**
+	 * constructeur.
+	 * @param initialX cordonnee x initial.
+	 * @param initialY  cordonne  y initial.
+	 */
 	public Position(final int initialX, final int initialY) {
 	  this.x = initialX;
 	  this.y = initialY;
 	}
-	
+	/**
+	 *  bouger par nbcase àpartir de position x.
+	 * @param nbCases nombre de case à avancer
+	 */
 	public void moveX(final int nbCases) {
 	      this.x += nbCases;
 	}
+	/**
+	 *  bouger par nbcase à partir de position y.
+	 * @param nbCases nombre de case à avancer
+	 */
 	public void moveY(final int nbCases) {
 	      this.y += nbCases;
 	}
+	/**
+	 * acceder à la position x.
+	 * @return coordonne x du position actuelle.
+	 */
 	
 	public int getX() {
 	      return this.x;
 	}
-
+    /**
+     * acceder à la aposition y .
+     * @return cordonne y du position actuelle.
+     */
 	public int getY() {
 	      return this.y;
 	}
--- src/main/java/uvsq21920965/Robot.java
+++ src/main/java/uvsq21920965/Robot.java
@@ -1,36 +1,49 @@
 package uvsq21920965;
-
+/**
+ * 
+ * @author sarra belmahdi.
+ *
+ */
 public class Robot extends RobotStatique {
-	
+	/**
+	 * constructeur.
+	 * @param pos position initiale.
+	 * @param dict direction initale.
+	 */
 	public Robot(final Position pos, final Direction dict) {
 	   super(pos, dict);
 	}
-
+    /**
+     * avancer le robot par une case.
+     */
 	@Override
 	public void avance() {
 	    switch (this.getDirection()) {
 	        case North:
 	            this.getPosition().moveY(1);
-	        case South:
-	            this.getPosition().moveY(-1);
 	        case East:
 	            this.getPosition().moveX(1);
 	        case West:
 	            this.getPosition().moveX(-1);
+	        case South:
+	            this.getPosition().moveY(-1);
 	        default:
 
 	    }
 	}
+	/**
+	 * tourner le robote 1/4 de tour.
+	 */
 	public void tourne() {
 	    switch (this.getDirection()) {
 	    case North:
 	        this.setDirection(Direction.East);
-	    case South:
-	        this.setDirection(Direction.West);
 	    case East:
 	        this.setDirection(Direction.North);
 	    case West:
 	        this.setDirection(Direction.South);
+	    case South:
+	        this.setDirection(Direction.West);
 	    default:
 	}
 	}
